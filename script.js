9b50e4719f3b2a7264212431c49e069ffb90c562
Adabanya Christian <adabanyachristian21@gmail.com>
2024-07-04 21:31:02 +0100

    updated the ui

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function displayTime() {
   let minutes = time.getUTCMinutes();
   let seconds = time.getUTCSeconds();
   let meridian = time.getUTCHours() < 12;
-  standardTimeFormat = `${hours.toString().padStart(2, "0")}:${minutes}:${seconds.toString().padStart(2, "0")}  ${meridian ? 'AM' : 'PM'}`;
+  standardTimeFormat = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}  ${meridian ? 'AM' : 'PM'}`;
  // 4 day
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const dayToday = daysOfWeek[time.getUTCDay()];
